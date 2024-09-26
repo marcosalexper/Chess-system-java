03db7611272c521f46edbf260434be35095d0b88
marcosalexper <marcosalexper@hotmail.com>
2024-09-26 11:28:08 -0300

    feat: classe teste

--- ChessSystem-Java/src/main/java/application/Program.java
+++ ChessSystem-Java/src/main/java/application/Program.java
@@ -4,10 +4,18 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
- * @author Win11
+ * @author Marcos Alexandre Pereira
  */
 public class Program {
+    
+    public static void main(String[] args){
+        
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+    }
     
 }
