3dc642bd3a5ba6ade19e5700541dffdc911db5a9
marcosalexper <marcosalexper@hotmail.com>
2024-09-26 11:40:12 -0300

    feat: classes Board e Piece criadas

---
+++ ChessSystem-Java/src/main/java/boardgame/Board.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author Marcos Alexandre Pereira
+ */
+public class Board {
+    
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+    
+    
+    
+}
---
+++ ChessSystem-Java/src/main/java/boardgame/Piece.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author Marcos Alexandre Pereira
+ */
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
