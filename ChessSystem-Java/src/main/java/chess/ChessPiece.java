6b83e7eb75c2930c444632b134f5ac390ad5dd49
marcosalexper <marcosalexper@hotmail.com>
2024-09-27 11:10:32 -0300

    feat: imprimindo o tabuleiro

---
+++ ChessSystem-Java/src/main/java/chess/ChessPiece.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+/**
+ *
+ * @author Marcos Alexandre Pereira
+ */
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Color color, Board board) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
